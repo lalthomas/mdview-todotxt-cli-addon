c7d0df3d008a0d4625450413caff0234060343c2
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-02 22:25:12 +0530

    add run file
    - for windows

--- mdview run.bat
+++ mdview run.bat
@@ -4,3 +4,11 @@ REM Creation Date : 2017-01-02
 REM Author : Lal Thomas 	
 REM Original File : D:\Dropbox\project\20150211-mdview todotxt cli addon\mdview 	
 	
+%~d0
+cd "%~p0"
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+pushd "%userProfile%\.todo.actions.d"
+echo "%scriptFolderPath%"
+mklink /d mdview "%scriptFolderPath%"
+popd
