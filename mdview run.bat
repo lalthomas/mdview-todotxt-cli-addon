67fa5f37fe71a6284a89db417f3bc64d63d7193d
Îalthomas <lal.thomas.mail@gmail.com>
2020-08-01 16:56:20 +0700

    Update mdview run.bat

--- mdview run.bat
+++ mdview run.bat
@@ -1,8 +1,7 @@
 @echo OFF 	
 REM File : mdview run.bat 	
 REM Creation Date : 2017-01-02 	
-REM Author : Lal Thomas 	
-REM Original File : D:\Dropbox\project\20150211-mdview todotxt cli addon\mdview 	
+REM Author : Lal Thomas 		
 	
 %~d0
 cd "%~p0"
